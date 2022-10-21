5d85eecdfa88ef48d80f8e43a0987fdccffa5ae6
Marcelo Pereira <marcelopereiracelo0@gmail.com>
2022-10-21 04:22:16 +0000

    quase lá

--- paginas/urls.py
+++ paginas/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
-from .views import PaginaInicial, Vaquinha, Ajuda
+from .views import PaginaInicial, PecaAjuda, Contato, Login, Doar
 #tm que ser urlpatterns pq é padrão django
 urlpatterns = [
     # path('endereço/', minhaview.as_view(), name = 'nome-da-url'),
     path('', PaginaInicial.as_view(), name = 'index'),
-    path('vaquinha/', Vaquinha.as_view(), name = 'vaquinha'),
-    path('ajuda/', Ajuda.as_view(), name = 'ajuda')
+    path('peça_ajuda/', PecaAjuda.as_view(), name = 'peca ajuda'),
+    path('contato/', Contato.as_view(), name = 'contato'),
+    path('login/', Login.as_view(), name = 'login'),
+    path('doar/', Doar.as_view(), name = 'doar'),
+
 ]
--- paginas/views.py
+++ paginas/views.py
@@ -1,9 +1,14 @@
 from django.views.generic import TemplateView
 
 #criando 
+
 class PaginaInicial(TemplateView):
     template_name = 'paginas/index.html'
-class Vaquinha(TemplateView):
-    template_name = 'paginas/vaquinha.html'
-class Ajuda(TemplateView):
-    template_name = 'paginas/help.html'
+class PecaAjuda(TemplateView):
+    template_name = 'paginas/peça_ajuda.html'
+class Contato(TemplateView):
+    template_name = 'paginas/contato.html'
+class Login(TemplateView):
+    template_name = 'paginas/login.html'
+class Doar(TemplateView):
+    template_name = 'paginas/doar.html'
